6c548d53932a84b36e7dd1114a6c17dfcd10d737
mikewheelmaker <radumihairotariu@gmail.com>
2020-11-11 17:39:33 +0200

    Test #11

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ anisoara.get('/', (req, res) =>  {
 });
 
 anisoara.get('/home', (req, res) => {
-	res.render('homeAnisoara');
+	res.render(require.resolve('./views/viewsA/homeAnisoara'));
 });
 
 anisoara.get('/testeADM', (req, res) => {

86275ebba2b8078750c7ab0eeb18ce7ce196da69
mikewheelmaker <radumihairotariu@gmail.com>
2021-01-13 16:07:44 +0200

    Get #5

--- app.js
+++ app.js
@@ -248,7 +248,7 @@ radu.post('/resultSerebryakov', (req, res) => {
 });
 
 radu.get('/getListaPersoane', (req, res) => {
-	return JSON.parse(listPersons);
+	res.render('viewsR/displayList', {list: listPersons, layout: 'layoutR'})
 })
 
 app.listen(port, hostname, () => console.log(`Serverul rulează la adresa http://${hostname}`));

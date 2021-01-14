d69a92edaa2c82f506cfb95ddd9dcc9ecd5c3ba7
mikewheelmaker <radumihairotariu@gmail.com>
2021-01-14 03:00:10 +0200

    Revert "Get #3"
    This reverts commit db701e8a53984e4a77c2f73c82592442a98794b2.

--- app.js
+++ app.js
@@ -248,7 +248,7 @@ radu.post('/resultSerebryakov', (req, res) => {
 });
 
 radu.get('/getListaPersoane', (req, res) => {
-	return JSON(listPersons);
+	return listPersons;
 })
 
 app.listen(port, hostname, () => console.log(`Serverul rulează la adresa http://${hostname}`));
